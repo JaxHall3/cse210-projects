1c3e17359ad8bdd699345971747770efc219406d
JaxHall3 <144852171+JaxHall3@users.noreply.github.com>
2023-09-20 02:57:40 +0000

    Succesfully tested and verified working.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace NumberListAnalyzer
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    class Program{
+        static void Main(string[]args)
+        {
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                if (input == 0)
+                    break;
+                numbers.Add(input);
+            }
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("NO numbers entered.");
+            }
+            else
+            {
+                int sum = 0;
+                int max = numbers[0];
+                
+                foreach (int num in numbers)
+                {
+                    sum += num;
+
+                    if (num > max)
+                    {
+                        max = num;
+                    }
+                }
+
+                double average = (double)sum / numbers.Count;
+
+                Console.WriteLine($"The sum is: {sum}");
+                Console.WriteLine($"The average is: {average}");
+                Console.WriteLine($"The largest number is: {max}");
+            }
+        }
     }
 }

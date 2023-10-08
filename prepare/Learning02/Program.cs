4b7f41b6236ddb8102f9f0a001609f4167fe346f
JaxHall3 <144852171+JaxHall3@users.noreply.github.com>
2023-10-08 03:33:07 +0000

    Completed Journal Project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,24 @@
-using System;
-
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create a new job instance named job1.
+        Job job1 = new Job("ABC Inc.", "Software Engineer", 2015, 2018);
+
+        // Set the member variables using dot notation.
+        job1._company = "XYZ Corp"; // Updating the company for job1
+
+        // Verify that you can display the company of this job.
+        Console.WriteLine("Company for job1: " + job1._company);
+
+        // Create a second job, set its variables.
+        Job job2 = new Job("DEF Ltd.", "Data Analyst", 2019, 2021);
+
+        // Display the company for the second job.
+        Console.WriteLine("Company for job2: " + job2._company);
+
+        // Test the Display method of the Job class.
+        job1.Display();
+        job2.Display();
     }
 }

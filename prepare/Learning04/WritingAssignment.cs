70e31ee60f33ca178ff9966505851e941b561d2a
JaxHall3 <144852171+JaxHall3@users.noreply.github.com>
2023-10-24 23:27:31 +0000

    finish Inheritance program,Assignment Parent Class

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,20 @@
+public class WritingAssignment : Assignment
+{
+    public string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+        {
+            _title = title;
+        }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {_GetStudentName()}";
+    }
+
+    public string _GetStudentName()
+    {
+        return GetSummary().Split(" - ")[0];
+    }
+}

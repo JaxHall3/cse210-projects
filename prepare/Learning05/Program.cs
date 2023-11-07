9a8ffe36249aa468b154d1a98f74da5d8c1fa903
JaxHall3 <144852171+JaxHall3@users.noreply.github.com>
2023-11-07 22:05:09 +0000

    Completed Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Green", 4, 6));
+        shapes.Add(new Circle("Blue", 3));
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.GetType().Name}, Color: {shape.Color}, Area: {shape.GetArea():F2}");
+        }
     }
 }
